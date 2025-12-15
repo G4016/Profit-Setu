81dd177b10324c74880d5850ee0772e1035f54f9
G4016 <ganeshnaik4016@gmail.com>
2025-12-16 03:37:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,26 @@ app.use(express.json());
    DATABASE
 ========================= */
 
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
 
 // Test DB connection
 pool.query("SELECT 1")
