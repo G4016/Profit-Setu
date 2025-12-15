a65bfa7994c51c255e8c4e9adeb0e211faf3d6fe
G4016 <ganeshnaik4016@gmail.com>
2025-12-16 02:57:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mysql from "mysql2/promise";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
+
 
 const app = express();
 app.use(cors());
